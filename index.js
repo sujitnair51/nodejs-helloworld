a565403d57187c513db8edc78355d1e45af61633
sujitnair51 <sujitnair51@gmail.com>
2021-11-04 13:37:50 -0400

    add corp support

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 var express = require('express');
+var cors = require('cors')
 app = express();
 
+var corsOptions = {
+    origin: 'http://localhost:8080',
+    optionsSuccessStatus: 200, // For legacy browser support
+    methods: "GET, PUT"
+}
+
+app.use(cors(corsOptions));
+// app.use(cors);
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 app.get('/', function (req, res) {
-    res.send('This is an updated version of Hello World!');
+    var json = JSON.stringify({ 
+        author: "testing", 
+        book: "QA"
+      });
+    res.send(json);
 });
 
 app.listen(8080, function () {
